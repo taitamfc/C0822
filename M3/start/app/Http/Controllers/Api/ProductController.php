a50a37f97ac0817ba51259dd731fb24c3275fb9c
taitamfc <taitam8590@gmail.com>
2022-12-14 10:50:59 +0700

    Web Service and RESTful - Rest

--- M3/start/app/Http/Controllers/Api/ProductController.php
+++ M3/start/app/Http/Controllers/Api/ProductController.php
@@ -17,12 +17,45 @@ class ProductController extends Controller
         return response()->json($item,200);
     }
     public function store(Request $request){
-        
+        // Luu
+        $product = new Product();
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->category_id = $request->category_id;
+        $product->image = 'images/iphone-14.png';
+        try {
+            $product->save();
+            return response()->json($product, 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 200);
+        }
     }
     public function update($id,Request $request){
-        
+
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->price = $request->price;
+
+        try {
+            $product->save();
+            return response()->json($product, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 200);
+        }
     }
     public function destroy($id){
-
+        $product = Product::find($id);
+        try {
+            $product->delete();
+            return response()->json($product, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 200);
+        }
     }
 }
